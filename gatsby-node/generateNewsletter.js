f9c3f9d02f4b143fbeea63a6d3c31ec61cc75b2d
Harry Solovay <hsolova@amazon.com>
2019-05-14 17:45:03 -0700

    fix(newsletter): generating most recent newsletter –– temporary fix

--- gatsby-node/generateNewsletter.js
+++ gatsby-node/generateNewsletter.js
@@ -16,21 +16,9 @@ const {
 const {templatePaths} = require('./constants');
 
 const getSlug = date => {
-  // const year = date.getFullYear();
-  // const week = date.getMonth() * 4 + Math.floor((date.getDate() - 1) / 7) + 1;
-
-  const d = new Date(
-    Date.UTC(date.getFullYear(), date.getMonth(), date.getDate()),
-  );
-  // Set to nearest Thursday: current date + 4 - current day number
-  // Make Sunday's day number 7
-  d.setUTCDate(d.getUTCDate() + 4 - (d.getUTCDay() || 7));
-  // Get first day of year
-  const yearStart = new Date(Date.UTC(d.getUTCFullYear(), 0, 1));
-  // Calculate full weeks to nearest Thursday
-  const weekNo = Math.ceil(((d - yearStart) / 86400000 + 1) / 7);
-  // Return array of year and week number
-  return join('/', ['newsletters', d.getUTCFullYear(), weekNo]);
+  const year = date.getFullYear();
+  const week = date.getMonth() * 4 + Math.floor((date.getDate() - 1) / 7) + 1;
+  return join('/', ['newsletters', year, week]);
 };
 
 const addWeeks = curry((numWeeks, date) => {
@@ -48,7 +36,7 @@ const addDays = curry((numDays, date) => {
 const getDateRange = ([year, week]) => {
   const date = new Date(year);
   const startDate = addWeeks(week, date);
-  const endDate = addDays(-2, addWeeks(1, startDate));
+  const endDate = addDays(-1, addWeeks(1, startDate));
   return [startDate, endDate];
 };
 
@@ -69,7 +57,9 @@ module.exports = (createPage, {events, posts, newsletterInjections}) => {
     contentBySlug[slug][category][id] = true;
   });
 
-  const currentDate = new Date();
+  const lastMonday = new Date();
+  const day = lastMonday.getDay();
+  day > 0 && lastMonday.setDate(lastMonday.getDate() - day + 1);
 
   forEachObjIndexed(
     ({edges}, category) => {
@@ -83,7 +73,7 @@ module.exports = (createPage, {events, posts, newsletterInjections}) => {
         switch (category) {
           case 'posts': {
             const slug = date && getSlug(date);
-            addWeeks(-2, currentDate) > date && placeCategory(slug, id);
+            date < lastMonday && placeCategory(slug, id);
             break;
           }
 
@@ -92,8 +82,7 @@ module.exports = (createPage, {events, posts, newsletterInjections}) => {
               i => {
                 const adjustedDate = addWeeks(-1 * i, date);
                 const slug = getSlug(adjustedDate);
-                addWeeks(-2, currentDate) > adjustedDate &&
-                  placeCategory(slug, id);
+                adjustedDate < lastMonday && placeCategory(slug, id);
               },
               [1, 2, 3, 4],
             );
