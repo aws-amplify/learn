7814f701f65910557dbb89bb02017d9c411fc4a1
Harry Solovay <hsolova@amazon.com>
2019-05-16 15:56:55 -0700

    chore(events template): creating expanded event detail

--- src/templates/Event.js
+++ src/templates/Event.js
@@ -1,73 +1,50 @@
 import {graphql} from 'gatsby';
-import Img from 'gatsby-image';
+import {track, extract} from '~/utilities';
+// import Img from 'gatsby-image';
+import {Text, Meta, Layout, Nav, Card} from '~/components';
 
 export const pageQuery = graphql`
   query($slug: String!) {
     markdownRemark(fields: {slug: {eq: $slug}}) {
-      frontmatter {
-        title
-        description
-        avatar {
-          childImageSharp {
-            fixed(width: 40, height: 40) {
-              ...GatsbyImageSharpFixed
-            }
-          }
-        }
-        href
-        tags
-      }
-      fields {
-        date(formatString: "MMMM DD, YYYY")
-        attendants {
-          frontmatter {
-            name
-            avatar {
-              childImageSharp {
-                fixed(width: 40, height: 40) {
-                  ...GatsbyImageSharpFixed
-                }
-              }
-            }
-          }
-        }
-      }
       html
+      ...Event
+      frontmatter {
+        avatar {
+          ...AvatarSmall
+        }
+      }
     }
   }
 `;
 
-export default ({data}) => {
-  const {fields, frontmatter, html} = data.markdownRemark;
+export default props => {
+  track.internalPageView(props);
+
+  const {html, fields, frontmatter} = extract.fromPath(
+    ['data', 'markdownRemark'],
+    props,
+  );
+
   const {date} = fields;
-  const {title, href, description, banner, organizers, tags} = frontmatter;
-  const {sizes} = banner.childImageSharp;
+  const {title, description, city, country, href} = frontmatter;
+
+  const main = (
+    <div>
+      <div>
+        <Text h2>{title}</Text>
+        <Text h4>{description}</Text>
+        <Text h4>{date}</Text>
+        <Text h4>{`${city}, ${country}`}</Text>
+        {/* eslint-disable-next-line */}
+        <div dangerouslySetInnerHTML={{__html: html}} />
+      </div>
+    </div>
+  );
 
   return (
-    <div>
-      Title:
-      {title}
-      <br />
-      Link to event:
-      <a href={href}>{href}</a>
-      Description:
-      {description}
-      <br />
-      organizers:
-      {organizers.join(', ')}
-      <br />
-      <Img {...{sizes}} />
-      <br />
-      Date:
-      {date}
-      <br />
-      <br />
-      <br />
-      Tags:
-      {tags && tags.join(', ')}
-      <br />
-      {/* eslint-disable-next-line */}
-      <div dangerouslySetInnerHTML={{__html: html}} />
-    </div>
+    <>
+      <Meta pageName='Chat' />
+      <Layout.Basic header={<Nav />} {...{main}} />
+    </>
   );
 };
