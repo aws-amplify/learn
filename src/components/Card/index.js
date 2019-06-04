765d4107eab1a8310f18068efc3a1806f7afc062
Harry Solovay <hsolova@amazon.com>
2019-06-04 16:53:36 -0700

    feat(footer component): added link to terms and privacy policy

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,6 +1,7 @@
 import * as ViewAll from './ViewAll';
 import * as Post from './Post';
 export {ViewAll, Post};
+
 export {default as Contributor} from './Contributor';
 export {default as Event} from './Event';
 export {default as Newsletter} from './Newsletter';
