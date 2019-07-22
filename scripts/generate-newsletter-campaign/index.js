c24d42ba7b9569ce8d316b31511ab615081a5f29
Harry Solovay <hsolova@amazon.com>
2019-07-22 16:09:36 -0700

    fix(newsletter template): better template build system

--- scripts/generate-newsletter-campaign/index.js
+++ scripts/generate-newsletter-campaign/index.js
@@ -1,9 +1,23 @@
-const path = require('path');
-const fs = require('fs');
-const R = require('ramda');
-const {parse} = require('parse5');
-const wrap = require('./wrap');
+const {configStyleValidator, renderEmail} = require('react-html-email');
+require('@babel/register')({presets: ['@babel/preset-env', 'react-app']});
 const {Pinpoint} = require('aws-sdk');
+const Email = require('./components/Email');
+const gather_data = require('./gather_data');
+
+configStyleValidator({
+  strict: true,
+  warn: true,
+  platforms: [
+    'gmail',
+    'gmail-android',
+    'apple-mail',
+    'apple-ios',
+    'yahoo-mail',
+    'outlook',
+    'outlook-legacy',
+    'outlook-web',
+  ],
+});
 
 const pinpoint = new Pinpoint();
 const ApplicationId = '75ab168484d64fdfb28c84ba1fb23523';
@@ -13,18 +27,17 @@ const day = today.getDate();
 const month = today.getMonth();
 const year = today.getFullYear();
 
-const today_is_monday = today.getDay() === 3;
+const today_is_monday = today.getDay() === 0;
 
 if (today_is_monday) {
   (async () => {
     const {CampaignsResponse = null} = await pinpoint
-      .getCampaigns({
-        ApplicationId,
-      })
+      .getCampaigns({ApplicationId})
       .promise();
 
     if (CampaignsResponse) {
       const {Item: campaigns = null} = CampaignsResponse;
+
       if (campaigns) {
         const already_created = campaigns.some(c => {
           const {CreationDate} = c;
@@ -40,99 +53,13 @@ if (today_is_monday) {
         });
 
         if (!already_created) {
-          const newsletter_years_path = path.join(
-            __dirname,
-            '../../public/newsletters',
-          );
-
-          const get_largest_number = s =>
-            Math.max(
-              ...R.filter(R.complement(R.equals(NaN)), R.map(parseInt, s)),
-            );
-
-          const newsletter_years_dirs = fs.readdirSync(newsletter_years_path);
-          const newsletter_year = get_largest_number(newsletter_years_dirs);
-          const newsletter_year_path = path.join(
-            newsletter_years_path,
-            String(newsletter_year),
-          );
-          const newsletter_year_dirs = fs.readdirSync(newsletter_year_path);
-          const newsletter_week = get_largest_number(newsletter_year_dirs);
-          const newsletter_path = path.join(
-            newsletter_year_path,
-            String(newsletter_week),
-            'index.html',
-          );
-          const newsletter_contents = fs.readFileSync(newsletter_path, 'utf-8');
-          const parsed = parse(newsletter_contents, {scriptingEnabled: false});
-
-          const safe_escapable_traverse = (node, callback) => {
-            const queue = [node];
-            while (queue.length !== 0) {
-              const current = queue.pop();
-
-              if (R.is(Object, current)) {
-                for (const i in current) {
-                  if (Object.prototype.hasOwnProperty.call(current, i)) {
-                    const result = callback(current[i]);
-                    if (result) return result;
-                    if (i !== 'parentNode') queue.unshift(current[i]);
-                  }
-                }
-              }
-            }
-          };
-
-          let date_range;
-          const roots = {
-            events: null,
-            posts: null,
-          };
-
-          safe_escapable_traverse(parsed, node => {
-            if (node.attrs) {
-              for (const attr of node.attrs) {
-                if (attr.name === 'data-date-range') date_range = attr.value;
-                if (attr.name === 'data-visitor-cue') roots[attr.value] = node;
-                if (date_range && roots.events && roots.posts) return;
-              }
-            }
-          });
-
-          const data = {
-            events: [],
-            posts: [],
-          };
-
-          R.mapObjIndexed((root, category) => {
-            safe_escapable_traverse(root, node => {
-              if (node.attrs) {
-                for (const attr of node.attrs) {
-                  if (attr.name === 'data-visitor-target') {
-                    const entry = Object.assign(
-                      {},
-                      ...node.attrs.map(({name, value}) => ({[name]: value})),
-                    );
-                    delete entry.class;
-                    delete entry['data-visitor-target'];
-                    data[category].push(entry);
-                  }
-                }
-              }
-            });
-          }, roots);
-
-          const HtmlBody = wrap(
-            newsletter_year,
-            newsletter_week,
-            date_range,
-            data,
-          );
+          const data = gather_data();
+          const email = Email(data);
+          const contents = renderEmail(email);
+          const {week: newsletter_week, year: newsletter_year} = data;
           const campaign_title = `Weekly Newsletter (week ${String(
             newsletter_week,
           )}, year ${String(newsletter_year)})`;
-          const in30Seconds = new Date();
-          in30Seconds.setSeconds(in30Seconds.getSeconds() + 30);
           pinpoint
             .createCampaign({
               ApplicationId,
@@ -142,12 +69,12 @@ if (today_is_monday) {
                   EmailMessage: {
                     Title: 'Amplify Weekly Newsletter',
                     FromAddress: 'harrysolovay@gmail.com',
-                    HtmlBody,
+                    HtmlBody: contents,
                   },
                 },
                 Name: campaign_title,
                 Schedule: {
-                  StartTime: in30Seconds.toISOString(),
+                  StartTime: new Date().toISOString(),
                   Frequency: 'ONCE',
                 },
                 SegmentId: '3ea09cde6bab4d49835c19236b7c1aab',
