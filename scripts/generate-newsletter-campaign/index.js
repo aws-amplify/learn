2c2e98379f4824847042d28ae615f6c86e6ef9bc
Harry Solovay <hsolova@amazon.com>
2019-07-23 12:43:29 -0700

    fix(scripts): testing envCache in console

--- scripts/generate-newsletter-campaign/index.js
+++ scripts/generate-newsletter-campaign/index.js
@@ -1,11 +1,11 @@
 const {configStyleValidator, renderEmail} = require('react-html-email');
 require('@babel/register')({presets: ['@babel/preset-env', 'react-app']});
 const {Pinpoint} = require('aws-sdk');
-const fs = require('fs');
-const path = require('path');
+// const fs = require('fs');
+// const path = require('path');
+const {execSync} = require('child_process');
 const Email = require('./components/Email');
 const gather_data = require('./gather_data');
-const {execSync} = require('child_process');
 
 configStyleValidator({
   strict: true,
@@ -31,8 +31,8 @@ configStyleValidator({
 //   process.exit(1);
 // })();
 
-// const pinpoint = new Pinpoint();
-// const ApplicationId = '75ab168484d64fdfb28c84ba1fb23523';
+const pinpoint = new Pinpoint();
+const ApplicationId = '75ab168484d64fdfb28c84ba1fb23523';
 
 const d = new Date();
 const today_is_monday = d.getDay() === 2;
@@ -46,76 +46,51 @@ if (today_is_monday) {
     [year, month, day].map(String).join('x'),
   ].join('');
 
-  execSync(`envCache --set ${key} 0`, {
-    stdio: 'inherit',
-  });
+  const from_cache = execSync(`envCache --get ${key}`)
+    .toString()
+    .trim();
+  const already_created = !!(from_cache && Number(from_cache));
 
-  // const get_envCache_response = execSync(`envCache --get ${key}`)
-  //   .toString()
-  //   .trim();
+  if (already_created) {
+    (async () => {
+      const data = gather_data();
+      const email = Email(data);
+      const contents = renderEmail(email);
+      const {week: newsletter_week, year: newsletter_year} = data;
+      const campaign_title = `Amplify Weekly Newsletter (week ${String(
+        newsletter_week,
+      )}, year ${String(newsletter_year)})`;
 
-  // if (!get_envCache_response) {
-  //   const set_envCache_response = execSync(`envCache --set ${key} 1`, {
-  //     stdio: 'inherit',
-  //   });
-  //   console.log('set_envCache_response', set_envCache_response);
-  // }
+      try {
+        const response = await pinpoint
+          .createCampaign({
+            ApplicationId,
+            WriteCampaignRequest: {
+              Description: campaign_title,
+              MessageConfiguration: {
+                EmailMessage: {
+                  Title: campaign_title,
+                  FromAddress: 'harrysolovay@gmail.com',
+                  HtmlBody: contents,
+                },
+              },
+              Name: campaign_title,
+              Schedule: {
+                StartTime: new Date().toISOString(),
+                Frequency: 'ONCE',
+              },
+              SegmentId: '3ea09cde6bab4d49835c19236b7c1aab',
+            },
+          })
+          .promise();
 
-  process.exit(1);
-
-  // (async () => {
-  //   const {CampaignsResponse = null} = await pinpoint
-  //     .getCampaigns({ApplicationId})
-  //     .promise();
-
-  //   if (CampaignsResponse) {
-  //     const {Item: campaigns = null} = CampaignsResponse;
-
-  //     if (campaigns) {
-  //       const already_created = campaigns.some(c => {
-  //         const {CreationDate} = c;
-  //         const comparison = new Date(CreationDate);
-  //         const comparison_day = comparison.getDate();
-  //         const comparison_month = comparison.getMonth();
-  //         const comparison_year = comparison.getFullYear();
-  //         return (
-  //           day === comparison_day &&
-  //           month === comparison_month &&
-  //           year === comparison_year
-  //         );
-  //       });
-
-  //       if (!already_created) {
-  //         const data = gather_data();
-  //         const email = Email(data);
-  //         const contents = renderEmail(email);
-  //         const {week: newsletter_week, year: newsletter_year} = data;
-  //         const campaign_title = `Weekly Newsletter (week ${String(
-  //           newsletter_week,
-  //         )}, year ${String(newsletter_year)})`;
-  //         pinpoint
-  //           .createCampaign({
-  //             ApplicationId,
-  //             WriteCampaignRequest: {
-  //               Description: campaign_title,
-  //               MessageConfiguration: {
-  //                 EmailMessage: {
-  //                   Title: 'Amplify Weekly Newsletter',
-  //                   FromAddress: 'harrysolovay@gmail.com',
-  //                   HtmlBody: contents,
-  //                 },
-  //               },
-  //               Name: campaign_title,
-  //               Schedule: {
-  //                 StartTime: new Date().toISOString(),
-  //                 Frequency: 'ONCE',
-  //               },
-  //               SegmentId: '3ea09cde6bab4d49835c19236b7c1aab',
-  //             },
-  //           })
-  //           .promise();
-  //       }
-  //     }
-  //   }
-  // })();
+        execSync(`envCache --set ${key} 1`, {
+          stdio: 'inherit',
+        });
+        console.log('Pinpoint campaign creation success: ', response);
+      } catch (e) {
+        console.log('Pinpoint campaign creation error: ', e);
+      }
+    })();
+  }
 }
