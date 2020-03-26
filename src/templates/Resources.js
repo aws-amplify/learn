e9f7b993be0e09d7237dcace7e1b309cfb578062
Dabit <dabit@8c85901b6e1c.ant.amazon.com>
2020-03-26 05:53:34 -0700

    added new workshops

--- src/templates/Resources.js
+++ src/templates/Resources.js
@@ -13,7 +13,7 @@ export default () => {
       <div css={container} class='items'>
         <div css={titleContainer}>
           <h1 class="text list-heading">Resources</h1>
-          <p css={baseTest}>Here you will find resources if you are interested in presenting a talk or a workshop at a conference or event. These workshops are also stand-alone, so if you can also take these and use them on your own, at your own pace.</p>
+          <p css={baseTest}>Here you will find resources like slide decks and workshops that will help you if you are interested in presenting a talk or a workshop at a conference or event. The workshops are also stand-alone, so if you can also take these and use them on your own, at your own pace.</p>
         </div>
         <div>
           <h2 class="text list-heading">Workshops</h2>
@@ -126,17 +126,68 @@ const tagContainer = css`
 `
 
 const workshops = [
-{ name: "Build a Photo-Sharing Web App with AWS Amplify and AWS AppSync", description: "In this workshop you'll learn how to build cloud-enabled mobile applications with React Native & AWS Amplify.", link: 'https://amplify-workshop.go-aws.com/', tags: ['GraphQL', 'Storage', 'API', 'React', 'Authentication', 'Amazon Cognito'] },
-{ name: "Building Cloud-enabled Mobile Applications with React Native & AWS Amplify", description: "In this workshop you'll learn how to build cloud-enabled mobile applications with React Native & AWS Amplify.", link: 'https://github.com/dabit3/aws-amplify-workshop-react-native', tags: ['React', 'React Native', 'API', 'GraphQL', 'AppSync', 'Storage', 'Authentication', 'Amazon Cognito'] },
-{ name: "Building real-time offline-ready Applications with React, GraphQL & AWS AppSync", description: "In this workshop we'll learn how to build cloud-enabled web applications with React, AppSync, GraphQL, & AWS Amplify.", link: 'https://github.com/dabit3/aws-appsync-react-workshop', tags: ['GraphQL', 'Storage', 'API', 'React', 'Hosting', 'AppSync'] }
+  {
+    name: "Building your first Fullstack Serverless App with Vue and AWS Amplify",
+    description: "In this workshop you'll learn how to build cloud-enabled web applications with Vue & AWS Amplify.",
+    link: "https://github.com/gsans/fullstack-serverless-amplify-vue",
+    tags: ['AppSync', 'GraphQL', 'Vue', 'Authentication', 'Amazon Cognito', 'Predictions', 'ML', 'AI', 'Hosting']
+  },
+{
+  name: "Build a Photo-Sharing Web App with AWS Amplify and AWS AppSync",
+  description: "In this workshop you'll learn how to build cloud-enabled mobile applications with React Native & AWS Amplify.",
+  link: 'https://amplify-workshop.go-aws.com/',
+  tags: ['GraphQL', 'Storage', 'API', 'React', 'Authentication', 'Amazon Cognito']
+},
+{
+  name: "Building Cloud-enabled Mobile Applications with React Native & AWS Amplify",
+  description: "In this workshop you'll learn how to build cloud-enabled mobile applications with React Native & AWS Amplify.",
+  link: 'https://github.com/dabit3/aws-amplify-workshop-react-native',
+  tags: ['React', 'React Native', 'API', 'GraphQL', 'AppSync', 'Storage', 'Authentication', 'Amazon Cognito']
+},
+{
+  name: "Building real-time offline-ready Applications with React, GraphQL & AWS AppSync",
+  description: "In this workshop we'll learn how to build cloud-enabled web applications with React, AppSync, GraphQL, & AWS Amplify.",
+  link: 'https://github.com/dabit3/aws-appsync-react-workshop',
+  tags: ['GraphQL', 'Storage', 'API', 'React', 'Hosting', 'AppSync']
+},
+{
+  name: "Building your first Fullstack Serverless Application with Angular and AWS Amplify",
+  description: "In this workshop you'll learn how to build cloud-enabled web applications with Angular & AWS Amplify.",
+  link: "https://github.com/gsans/fullstack-serverless-amplify-angular",
+  tags: ['AppSync', 'GraphQL', 'Angular', 'Authentication', 'Amazon Cognito', 'Predictions', 'ML', 'AI', 'Hosting']
+}
 ]
 
 const slideDecks = [
-  { name: 'Storage - Secure File and Image Hosting with Amazon S3', description: 'An introduction to the Storage category for AWS Amplify, both the client APIs as well as how to get up and running from the CLI', downloadPath: '../../slideDecks/storage.pptx', tags: ['Storage', 'Amazon S3'] },
-  { name: 'Auth - Implementing Authentication with AWS Amplify', description: 'A deep dive into the Auth category, mainly focusing on general web implementations.', downloadPath: '../../slideDecks/auth.pptx', tags: ['Authentication', 'Amazon Cognito'] },
-  { name: 'Introduction to Mobile Development on AWS', description: 'An overview of the current options and tools for mobile development from AWS Mobile and AWS Amplify.', downloadPath: '../../slideDecks/mobile_intro.pptx', tags: ['Mobile', 'iOS', 'Android'] },
-  { name: 'Hosting - Deploying Your Application with CI & CD', description: 'A deep dive into the AWS Amplify Console hosting platform.', downloadPath: '../../slideDecks/console.pptx', tags: ['Hosting', 'Amplify Console', 'CI / CD'] },
-  { name: 'Predictions - Adding Machine Learning to Your Apps with Zero Back End Code', description: 'How to integrate AWS managed machine learning services on your web or mobile app using AWS Amplify.', downloadPath: '../../slideDecks/predictions.pptx', tags: ['Predictions', 'ML', 'AI'] },
+  {
+    name: 'Storage - Secure File and Image Hosting with Amazon S3',
+    description: 'An introduction to the Storage category for AWS Amplify, both the client APIs as well as how to get up and running from the CLI', downloadPath: '../../slideDecks/storage.pptx',
+    tags: ['Storage', 'Amazon S3']
+  },
+  {
+    name: 'Auth - Implementing Authentication with AWS Amplify',
+    description: 'A deep dive into the Auth category, mainly focusing on general web implementations.',
+    downloadPath: '../../slideDecks/auth.pptx',
+    tags: ['Authentication', 'Amazon Cognito']
+  },
+  {
+    name: 'Introduction to Mobile Development on AWS',
+    description: 'An overview of the current options and tools for mobile development from AWS Mobile and AWS Amplify.',
+    downloadPath: '../../slideDecks/mobile_intro.pptx',
+    tags: ['Mobile', 'iOS', 'Android']
+  },
+  {
+    name: 'Hosting - Deploying Your Application with CI & CD',
+    description: 'A deep dive into the AWS Amplify Console hosting platform.',
+    downloadPath: '../../slideDecks/console.pptx',
+    tags: ['Hosting', 'Amplify Console', 'CI / CD']
+  },
+  {
+    name: 'Predictions - Adding Machine Learning to Your Apps with Zero Back End Code',
+    description: 'How to integrate AWS managed machine learning services on your web or mobile app using AWS Amplify.',
+    downloadPath: '../../slideDecks/predictions.pptx',
+    tags: ['Predictions', 'ML', 'AI']
+  },
   {
     name: 'The Future of Real-time | Offline | Data',
     description: 'An in depth look into Amplify DataStore, an persistent on-device storage repository and managed API service.',
