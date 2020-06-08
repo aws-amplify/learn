4bccc9b4bedf984137ac93398f0744be1f4f7c57
Harry Solovay <harrysolovay@gmail.com>
2020-06-08 10:53:29 -0700

    chore: upgrade deps, fix broken event links

--- src/components/Link.js
+++ src/components/Link.js
@@ -2,8 +2,9 @@ import {Link} from 'gatsby';
 import ExternalLink from './ExternalLink';
 
 export default props => {
-  const {href, to, ...rest} = props;
+  let {href, to, ...rest} = props;
   const external = !!href;
   const Tag = external ? ExternalLink : Link;
+  if (to && !to.startsWith("/")) to = `/${to}`;
   return <Tag {...rest} {...(external ? {href} : {to})} />;
 };
