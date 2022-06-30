92d0e126090f57fc65120bf49a20a517ee39cd4e
Tim Nguyen <timngyn@gmail.com>
2022-06-30 09:06:08 -0700

    Move AmplifyProvider into Layout component

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,5 +1,10 @@
 import Head from "next/head";
-import { Grid, ThemeProvider, useBreakpointValue } from "@aws-amplify/ui-react";
+import {
+  AmplifyProvider,
+  Grid,
+  ThemeProvider,
+  useBreakpointValue,
+} from "@aws-amplify/ui-react";
 import { useEffect, useState } from "react";
 import { studioTheme } from "../../ui-components";
 import { LearnFooter } from "../LearnFooter";
@@ -72,42 +77,44 @@ export function Layout({ children }: { children: any }) {
         />
       </Head>
       <ThemeProvider theme={studioTheme}>
-        <LearnNavBar isMobile={isMobile} />
-        <Grid
-          templateColumns={{
-            base: "1fr",
-            small: "1fr",
-            medium: "1fr",
-            large: "1fr min(120ch, 100%) 1fr",
-          }}
-          marginLeft={{
-            base: "32px",
-            small: "32px",
-            medium: "64px",
-            large: "129px",
-          }}
-          marginRight={{
-            base: "32px",
-            small: "32px",
-            medium: "64px",
-            large: "129px",
-          }}
-          marginTop={{
-            base: "32px",
-            small: "32px",
-            medium: "64px",
-            large: "128px",
-          }}
-          marginBottom={{
-            base: "114px",
-            small: "114px",
-            medium: "118px",
-            large: "143px",
-          }}
-        >
-          {children}
-        </Grid>
-        <LearnFooter isMobile={isMobile} />
+        <AmplifyProvider>
+          <LearnNavBar isMobile={isMobile} />
+          <Grid
+            templateColumns={{
+              base: "1fr",
+              small: "1fr",
+              medium: "1fr",
+              large: "1fr min(120ch, 100%) 1fr",
+            }}
+            marginLeft={{
+              base: "32px",
+              small: "32px",
+              medium: "64px",
+              large: "129px",
+            }}
+            marginRight={{
+              base: "32px",
+              small: "32px",
+              medium: "64px",
+              large: "129px",
+            }}
+            marginTop={{
+              base: "32px",
+              small: "32px",
+              medium: "64px",
+              large: "128px",
+            }}
+            marginBottom={{
+              base: "114px",
+              small: "114px",
+              medium: "118px",
+              large: "143px",
+            }}
+          >
+            {children}
+          </Grid>
+          <LearnFooter isMobile={isMobile} />
+        </AmplifyProvider>
       </ThemeProvider>
     </>
   );
