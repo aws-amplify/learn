a0956c48c79668966fa944370f60f72b143b4c5f
Tim Nguyen <timngyn@gmail.com>
2022-07-15 02:20:05 -0700

    Set html lang

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -9,7 +9,7 @@ import Document, {
 export default class MyDocument extends Document {
   render() {
     return (
-      <Html>
+      <Html lang="en">
         <Head>
         </Head>
         <body>
