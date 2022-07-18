5e082162e0fe3a6c334069848eeee9d3faa1fcba
Tim Nguyen <timngyn@gmail.com>
2022-07-18 11:17:30 -0700

    Update look of trailer modal

--- src/components/YoutubeEmbed/YoutubeEmbed.tsx
+++ src/components/YoutubeEmbed/YoutubeEmbed.tsx
@@ -1,17 +1,17 @@
 import { View } from "@aws-amplify/ui-react";
+import styles from "./YoutubeEmbed.module.scss";
 
 export function YoutubeEmbed({ embedId }: { embedId: string }) {
   return (
-    <View as="div">
+
       <iframe
-        width="100%"
-        height="450px"
+        className={styles["youtube-iframe"]}
         src={`https://www.youtube-nocookie.com/embed/${embedId}`}
         title="YouTube video player"
         frameBorder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
         allowFullScreen
       ></iframe>
-    </View>
+
   );
 }
