b8850e09e82d9ed895e3cd606f9f69ec212984cf
Tim Nguyen <timngyn@gmail.com>
2022-07-17 19:20:48 -0700

    Update navbar on mobile

--- src/components/LearnNavBar/LearnNavBarDesktop/LearnNavBarDesktop.tsx
+++ src/components/LearnNavBar/LearnNavBarDesktop/LearnNavBarDesktop.tsx
@@ -1,9 +1,9 @@
 import { Button, Flex, View } from "@aws-amplify/ui-react";
-import { forwardRef, useState } from "react";
-import { LearnLogo } from "../../ui-components";
-import ExternalIconCustom from "../../ui-components/ExternalIconCustom";
-import styles from "./LearnNavBarDesktop.module.scss";
+import { LearnLogo } from "../../../ui-components";
+import ExternalIconCustom from "../../../ui-components/ExternalIconCustom";
+import styles from "../LearnNavBar.module.scss";
 import Link from "next/link";
+import { NavBarMenuItem } from "../NavBarMenuItem";
 
 export function LearnNavBarDesktop() {
   return (
@@ -133,59 +133,4 @@ export function LearnNavBarDesktop() {
   );
 }
 
-const NavBarMenuItem = forwardRef(
-  (
-    {
-      children,
-      href,
-      showExternalIcon = false,
-    }: {
-      children: any;
-      href?: string;
-      showExternalIcon?: boolean;
-    },
-    ref
-  ) => {
-    // Colors for changing "ExternalIcon"
-    const hoverColor = "rgba(35,47,62,1)";
-    const defaultColor = "rgba(84,91,100,1)";
-    const [isHoverColor, setIsHoverColor] = useState(defaultColor);
 
-    return (
-      <a
-        className={styles["navbar-menu-item"]}
-        href={href}
-        {...(showExternalIcon
-          ? { rel: "noopener noreferrer", target: "_blank" }
-          : {})}
-      >
-        <Flex
-          alignItems="center"
-          onMouseEnter={() => {
-            if (showExternalIcon) {
-              setIsHoverColor(hoverColor);
-            }
-          }}
-          onMouseLeave={() => {
-            if (showExternalIcon) {
-              setIsHoverColor(defaultColor);
-            }
-          }}
-        >
-          {children}
-          <ExternalIconCustom
-            width="24px"
-            height="30px"
-            shrink="0"
-            position="relative"
-            padding="0px 0px 0px 0px"
-            color={isHoverColor}
-            display={showExternalIcon ? "" : "none"}
-          />
-        </Flex>
-      </a>
-    );
-  }
-);
-
-NavBarMenuItem.displayName = "NavBarMenuItem";
