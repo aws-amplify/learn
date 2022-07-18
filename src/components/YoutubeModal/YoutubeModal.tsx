4a050a85fdf0de63396ce0dac31ee65b89841201
Tim Nguyen <timngyn@gmail.com>
2022-07-18 11:17:30 -0700

    Update look of trailer modal

--- src/components/YoutubeModal/YoutubeModal.tsx
+++ src/components/YoutubeModal/YoutubeModal.tsx
@@ -1,10 +1,12 @@
+import { Flex, Text, useBreakpointValue, View } from "@aws-amplify/ui-react";
 import {
-  Flex,
-  Text,
-  Button,
-  useBreakpointValue,
-  View,
-} from "@aws-amplify/ui-react";
+  ClassAttributes,
+  HTMLAttributes,
+  ReactElement,
+  JSXElementConstructor,
+  ReactFragment,
+  ReactPortal,
+} from "react";
 import Modal from "react-modal";
 import { CloseIcon } from "../../ui-components";
 import { YoutubeEmbed } from "../YoutubeEmbed";
@@ -31,13 +33,82 @@ export function YoutubeModal({
     xl: "50vw",
   }) as string;
 
+  // Custom overlay element to hold the close button outside of the actual modal
+  const overlayElement = (
+    props: JSX.IntrinsicAttributes &
+      ClassAttributes<HTMLDivElement> &
+      HTMLAttributes<HTMLDivElement>,
+    contentElement:
+      | string
+      | number
+      | boolean
+      | ReactElement<any, string | JSXElementConstructor<any>>
+      | ReactFragment
+      | ReactPortal
+      | null
+      | undefined
+  ) => {
+    return (
+      <div
+        {...props}
+        // position="fixed"
+        // inset="0px"
+        // backgroundColor="rgba(0, 0, 0, 0.6)"
+      >
+        <View
+          as="div"
+          onClick={closeModal}
+          position="absolute"
+          right="20px"
+          top="20px"
+        >
+          <CloseIcon
+            color="white"
+            tabIndex="0"
+            role="button"
+            className={styles["close-modal"]}
+            ariaLabel="Close modal"
+            overrides={{
+              "Vector 3": {
+                // @ts-ignore
+                paths: [
+                  {
+                    d: "M1 0C1 -0.552285 0.552285 -1 0 -1C-0.552285 -1 -1 -0.552285 -1 0L1 0ZM-1 16C-1 16.5523 -0.552285 17 0 17C0.552285 17 1 16.5523 1 16L-1 16ZM-1 0L-1 16L1 16L1 0L-1 0Z",
+                    fillRule: "nonzero",
+                    strokeWidth: 2,
+                  },
+                ],
+              },
+              "Vector 4": {
+                // @ts-ignore
+                paths: [
+                  {
+                    d: "M1 0C1 -0.552285 0.552285 -1 0 -1C-0.552285 -1 -1 -0.552285 -1 0L1 0ZM-1 16C-1 16.5523 -0.552285 17 0 17C0.552285 17 1 16.5523 1 16L-1 16ZM-1 0L-1 16L1 16L1 0L-1 0Z",
+                    fillRule: "nonzero",
+                    strokeWidth: 2,
+                  },
+                ],
+              },
+            }}
+          />
+        </View>
+        {contentElement}
+      </div>
+    );
+  };
+
   return (
     <Modal
       isOpen={modalIsOpen}
       onRequestClose={closeModal}
+      overlayElement={overlayElement}
       style={{
+        overlay: {
+          backgroundColor: "rgba(0, 0, 0, 0.800)",
+        },
         content: {
           width: modalWidth,
+          padding: "0px",
           top: "50%",
           left: "50%",
           right: "auto",
@@ -48,48 +119,7 @@ export function YoutubeModal({
       }}
       contentLabel={`Course trailer for ${courseTitle}`}
     >
-      <Flex direction="column">
-        <Flex
-          direction="row"
-          justifyContent="space-between"
-          alignItems="center"
-        >
-          <Text
-            fontFamily="Amazon Ember Display"
-            fontWeight="300"
-            fontSize="1.5rem"
-          >{`Trailer for ${courseTitle}`}</Text>
-          <View as="div" onClick={closeModal}>
-            <CloseIcon
-              tabIndex="0"
-              role="button"
-              className={styles["close-modal"]}
-              ariaLabel="Close modal"
-              overrides={{
-                "Vector 3": {
-                  // @ts-ignore
-                  paths: [
-                    {
-                      d: "M1 0C1 -0.552285 0.552285 -1 0 -1C-0.552285 -1 -1 -0.552285 -1 0L1 0ZM-1 16C-1 16.5523 -0.552285 17 0 17C0.552285 17 1 16.5523 1 16L-1 16ZM-1 0L-1 16L1 16L1 0L-1 0Z",
-                      fillRule: "nonzero",
-                      strokeWidth: 2,
-                    },
-                  ],
-                },
-                "Vector 4": {
-                  // @ts-ignore
-                  paths: [
-                    {
-                      d: "M1 0C1 -0.552285 0.552285 -1 0 -1C-0.552285 -1 -1 -0.552285 -1 0L1 0ZM-1 16C-1 16.5523 -0.552285 17 0 17C0.552285 17 1 16.5523 1 16L-1 16ZM-1 0L-1 16L1 16L1 0L-1 0Z",
-                      fillRule: "nonzero",
-                      strokeWidth: 2,
-                    },
-                  ],
-                },
-              }}
-            />
-          </View>
-        </Flex>
+      <Flex>
         <YoutubeEmbed embedId={courseTrailerEmbedId || ""} />
       </Flex>
     </Modal>
