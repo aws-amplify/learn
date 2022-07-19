27ae2e69daaba39bd061b52c8b9e19bfcfc9c5cd
Tim Nguyen <timngyn@gmail.com>
2022-07-19 13:37:08 -0700

    Get featured course inside getServerSideProps

--- src/components/HomePageContent/HomePageContent.tsx
+++ src/components/HomePageContent/HomePageContent.tsx
@@ -1,35 +1,9 @@
-import { DataStore } from "aws-amplify";
-import { Flex, Grid, Placeholder } from "@aws-amplify/ui-react";
+import { Flex } from "@aws-amplify/ui-react";
 import { default as CardLayoutCollection } from "../../ui-components/CardLayoutCollectionCustom";
 import { Course } from "../../models";
-import { useCallback, useEffect, useState } from "react";
 import { HeroCourse } from "../HeroCourse";
-import { useFirstDatastoreQuery } from "../../hooks/useFirstDatastoreQuery";
 
-export function HomePageContent() {
-  const [heroCourse, setHeroCourse] = useState<Course | null>(null);
-  const [isLoading, setIsLoading] = useState(true);
-
-  async function queryHeroCourse() {
-    const featuredCourse = await DataStore.query(Course, (c) =>
-      c.isFeatured("eq", true)
-    );
-
-    if (featuredCourse.length === 1) {
-      setHeroCourse(featuredCourse[0]);
-      setIsLoading(false);
-    }
-  }
-
-  const queryHeroCourseCallback = useCallback(queryHeroCourse, []);
-
-  useFirstDatastoreQuery(queryHeroCourseCallback);
-
-  useEffect(() => {
-    // If we still don't have the heroCourse then try and query again
-    queryHeroCourseCallback();
-  }, [queryHeroCourseCallback]);
-
+export function HomePageContent({ heroCourse }: { heroCourse: Course }) {
   return (
     <Flex
       direction="column"
@@ -40,11 +14,7 @@ export function HomePageContent() {
         large: "124px",
       }}
     >
-      {isLoading ? (
-        <Placeholder size="large" isLoaded={!isLoading} />
-      ) : (
-        <HeroCourse course={heroCourse} />
-      )}
+      <HeroCourse course={heroCourse} />
       <CardLayoutCollection gap="40px" isOnHomePage={true} />
     </Flex>
   );
