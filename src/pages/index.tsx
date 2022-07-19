764528bbb333ab479787ac46f940558942648d52
Tim Nguyen <timngyn@gmail.com>
2022-07-19 13:37:08 -0700

    Get featured course inside getServerSideProps

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,19 @@
-import type { NextPage } from "next";
 import { Button, Card, Heading } from "@aws-amplify/ui-react";
 import { HomePageContent } from "../components/HomePageContent";
 import { ActionLayout } from "../components/ActionLayout";
 import { Layout } from "../components/Layout";
 import ExternalIconCustom from "../ui-components/ExternalIconCustom";
+import { withSSRContext } from "aws-amplify";
+import { Course } from "../models";
+import { serializeModel, deserializeModel } from "@aws-amplify/datastore/ssr";
 
-const Home: NextPage = () => {
+export default function Home(data: any) {
+  const featuredCourse = deserializeModel(Course, data.featuredCourse);
+
   return (
     <Layout>
       <Card columnStart="2">
-        <HomePageContent />
+        <HomePageContent heroCourse={featuredCourse} />
       </Card>
       <ActionLayout>
         <Card>
@@ -36,6 +40,27 @@ const Home: NextPage = () => {
       </ActionLayout>
     </Layout>
   );
-};
+}
+export async function getServerSideProps(context: any) {
+  const { DataStore } = withSSRContext(context);
 
-export default Home;
+  const featuredCourse = await DataStore.query(Course, (c: any) =>
+    c.isFeatured("eq", true)
+  );
+
+  if (featuredCourse.length === 1) {
+    return {
+      props: {
+        featuredCourse: serializeModel(featuredCourse[0]),
+      },
+    };
+  } else {
+    const firstCourse = await DataStore.query(Course);
+
+    return {
+      props: {
+        featuredCourse: serializeModel(firstCourse[0]),
+      },
+    };
+  }
+}
