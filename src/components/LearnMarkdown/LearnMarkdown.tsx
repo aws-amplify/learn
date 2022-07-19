7e6147cc454b6965594c4116a48549d843bc235c
Tim Nguyen <timngyn@gmail.com>
2022-07-19 13:46:41 -0700

    Add margin-left and margin-right auto for images in markdown so they are centered

--- src/components/LearnMarkdown/LearnMarkdown.tsx
+++ src/components/LearnMarkdown/LearnMarkdown.tsx
@@ -4,6 +4,7 @@ import rehypeExternalLinks from "rehype-external-links";
 import { Button, View } from "@aws-amplify/ui-react";
 import { useState } from "react";
 import { CopyToClipboard } from "react-copy-to-clipboard";
+import styles from "./LearnMarkdown.module.scss";
 
 function CodeBlock({
   node,
@@ -21,7 +22,7 @@ function CodeBlock({
   const [showButton, setShowButton] = useState(false);
   const [copied, setCopied] = useState(false);
 
-  // Custom wrapper around `code` 
+  // Custom wrapper around `code`
   const PreTag = ({ children, ...rest }: { children: any }) => (
     <View {...rest} as="div" backgroundColor="#F2F3F3" fontSize="0.9rem">
       {children}
@@ -77,6 +78,7 @@ export function LearnMarkdown({
       <ReactMarkdown
         rehypePlugins={[rehypeExternalLinks]}
         components={{ code: CodeBlock }}
+        className={styles["markdown-wrapper"]}
       >
         {markdownContent}
       </ReactMarkdown>
