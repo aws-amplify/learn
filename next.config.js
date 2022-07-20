dd664bc565374bdc13ae771a3882d8a8d77b15eb
Tim Nguyen <timngyn@gmail.com>
2022-07-20 08:57:39 -0700

    Add /learn basepath

--- next.config.js
+++ next.config.js
@@ -1,12 +1,13 @@
-const path = require('path');
+const path = require("path");
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   sassOptions: {
-    includePaths: [path.join(__dirname, 'src/styles')],
-    prependData: '@import "_variables.scss";'
+    includePaths: [path.join(__dirname, "src/styles")],
+    prependData: '@import "_variables.scss";',
   },
-}
+  basePath: "/learn",
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
