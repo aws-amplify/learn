b68d6a37d5cbd731e1ecdbec570c14aa9b801632
Tim Nguyen <timngyn@gmail.com>
2022-07-22 16:15:06 -0700

    Add fonts to Next Head

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,16 +1,10 @@
-import Document, {
-  DocumentContext,
-  Html,
-  Main,
-  NextScript,
-  Head
-} from "next/document";
-
+import Document, { Html, Main, NextScript, Head } from "next/document";
 export default class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
         <Head>
+          <link href="/src/styles/fonts.css" rel="stylesheet" />
         </Head>
         <body>
           <Main />
