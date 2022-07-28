dca64f0ce2896565a450e5f8444e63e20a91d284
Tim Nguyen <timngyn@gmail.com>
2022-07-27 21:29:05 -0700

    Remove console.logs

--- src/utils/track.ts
+++ src/utils/track.ts
@@ -9,12 +9,10 @@ declare global {
 
 export function configureAdobeAnalytics(): void {
   if (!configured) {
-    console.log("not configured yet!");
     if (
       typeof window !== "undefined" &&
       typeof window.AWSCShortbread !== "undefined"
     ) {
-      console.log("not configured yet! and AA exists!");
       window
         .AWSCShortbread({
           domain: ".amplify.aws",
