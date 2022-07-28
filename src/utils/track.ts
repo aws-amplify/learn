16bad9e3861d4f33bae32804b2ce3c5a0fff6bd7
Tim Nguyen <timngyn@gmail.com>
2022-07-28 12:20:07 -0700

    Add custom pageview event for adobe analytics

--- src/utils/track.ts
+++ src/utils/track.ts
@@ -1,9 +1,31 @@
 let configured = false;
 
+interface PageViewOptions {
+  event: Event;
+  page: Page;
+  data?: Record<string, any>;
+}
+
+interface CustomEventOptions {
+  event: Event;
+  data?: Record<string, any>;
+}
+
+interface Page {
+  pageURL: string;
+  pageType?: string;
+}
+
+interface Event {
+  name: string;
+  type: string;
+  description?: string;
+}
 declare global {
   interface Window {
     s: any;
     AWSCShortbread: any;
+    AWSMA: any;
   }
 }
 
@@ -25,4 +47,30 @@ export function configureAdobeAnalytics(): void {
       configured = true;
     }
   }
+}
+
+function trackCustomEvent(opt: CustomEventOptions): void {
+  try {
+    window.AWSMA.ready(() => {
+      document.dispatchEvent(
+        new CustomEvent(window.AWSMA.TRIGGER_EVENT, { detail: opt })
+      );
+    });
+  } catch (error) {
+    // don't want to error for analytics events
+  }
+}
+
+export function pageView(eventName: string, pageURL: string): void {
+  const opt: PageViewOptions = {
+    event: {
+      type: "pageview",
+      name: eventName,
+    },
+    page: {
+      pageURL: pageURL,
+    },
+  };
+
+  trackCustomEvent(opt);
 }
