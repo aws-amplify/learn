975b7317cb0c89ba422e546dd767c65678148cea
Tim Nguyen <timngyn@gmail.com>
2022-08-11 16:46:06 -0700

    Don't pass url to github issue

--- src/utils/createGithubIssueLink.ts
+++ src/utils/createGithubIssueLink.ts
@@ -1,12 +1,10 @@
 const NEW_GITHUB_ISSUE_LINK =
   "https://github.com/aws-amplify/community/issues/new";
 
-export function createGithubIssueLink(url: string): string {
+export function createGithubIssueLink(): string {
   const params = [
     "title=[Bug] TITLE_HERE",
-    `body=${encodeURIComponent(
-      `**Page**: ${url}\n\n**Issue**:\n\n<!-- Your comments here -->`
-    )}`,
+    `body=${encodeURIComponent(`**Issue**:\n\n<!-- Your comments here -->`)}`,
   ];
 
   return `${NEW_GITHUB_ISSUE_LINK}?${params.join("&")}`;
