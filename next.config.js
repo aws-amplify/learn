00fae9680ecc8cd8f965ed5704f576872f9066b4
Tim Nguyen <54393192+timngyn@users.noreply.github.com>
2022-09-28 13:40:12 -0700

    Add "learn" basepath (#364)
    * Use '/learn' as basePath property
    
    * Remove ISR and set fallback to false
    
    * Use next export
    
    * Update to only query for published courses
    
    * Update amplify.yml to point to out directory for next SSG
    
    * Remove base path
    
    * Add only the learn basepath
    
    * Add learn to public directory paths
    
    * Update paths for meta tags
    
    * Update CODEOWNERS
    
    * Remove unused import
    
    * Use environment variable for basepath
    
    * Use sassOptions to prepend basepath in scss
    
    * Only prepend sass data if basepath env var exists
    
    * Add empty string for sass basepath variable if it doesn't exist
    
    * Set trailingSlash to true to test 404 pages
    
    * Revert "Set trailingSlash to true to test 404 pages"
    
    This reverts commit 515b6ccf6119b2f58869688588b42bc42c8b6d4d.

--- next.config.js
+++ next.config.js
@@ -1,11 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  basePath: process.env.BASEPATH,
   typescript: {
     ignoreBuildErrors: true,
   },
+  sassOptions: {
+    prependData: process.env.BASEPATH
+      ? `$basepath: '${process.env.BASEPATH}';`
+      : `$basepath: '';`,
+  },
   env: {
     SITE_URL: process.env.SITE_URL,
+    BASEPATH: process.env.BASEPATH,
   },
 };
 
--- src/components/LearnNavBar/LearnNavBarMobile/LearnNavBarMobile.tsx
+++ src/components/LearnNavBar/LearnNavBarMobile/LearnNavBarMobile.tsx
@@ -6,17 +6,17 @@ export function LearnNavBarMobile() {
   return (
     <>
       <View padding="12px" className={styles["mobile-nav-menu-items"]}>
-        <Link href="/learn">
+        <Link href="/">
           <a className={styles["navbar-menu-item"]}>Home</a>
         </Link>
       </View>
       <View padding="12px" className={styles["mobile-nav-menu-items"]}>
-        <Link href="/learn/courses">
+        <Link href="/courses">
           <a className={styles["navbar-menu-item"]}>Courses</a>
         </Link>
       </View>
       <View padding="12px" className={styles["mobile-nav-menu-items"]}>
-        <Link href="/learn/about">
+        <Link href="/about">
           <a className={styles["navbar-menu-item"]}>About</a>
         </Link>
       </View>
